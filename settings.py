65044cd9a8eccc3719cd57a09f4a852a46239c99
Joachim.Wan <joachimwan@gmail.com>
2023-11-19 22:47:05 +0800

    placeholder for SETTINGS

--- settings.py
+++ settings.py
@@ -1,4 +1,9 @@
 from pathlib import Path
 
-
 BASE_DIR = Path.cwd()
+
+LATEST_LOOKAHEAD_DIR = BASE_DIR.joinpath('lookahead', '230101_NTP_Lookahead')
+
+OCS_DIR = BASE_DIR.joinpath('OCS')
+
+LATEST_DCCS_DIR = BASE_DIR.joinpath('DCCS', '230101_NTP_DCCS')
