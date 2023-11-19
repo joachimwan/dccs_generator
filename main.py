0b4e130a996afc87132bd82de4c6ad559b021025
Joachim.Wan <joachimwan@gmail.com>
2023-11-19 21:31:56 +0800

    added proposed workflow

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import settings
 
-
-def print_hi(name):
-    print(f'Hi, {name}')
-
+# C:\Users\Joachim.Wan\Desktop\OpsProject\dccs_generator
+# Proposed workflow:
+# - Adjust settings.py file to select data sources.
+# - Read the latest Excel Campaign Lookahead. Extract and verify information.
+# - Read all OCS and OCS revisions. Extract and verify information.
+# - Read all tariffs. Extract and verify information.
+# - Read the latest DCCS. Extract and verify information. Update manual inputs.
+# - Generate DCCS rows from validated OCS and tariffs.
+# - Generate charges based on charging mechanisms and Lookahead (actual and projected AFE days).
+# - Generate charges based on manual inputs.
 
 if __name__ == '__main__':
     pass
