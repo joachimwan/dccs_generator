69a7da428f925f62949cba8fd18faa2cd3f5a7b6
Joachim.Wan <joachimwan@gmail.com>
2023-11-23 21:54:37 +0800

    added start date projection

--- lookahead.py
+++ lookahead.py
@@ -21,13 +21,15 @@ from settings import *
 # - Identify latest lookahead. Ensure lookahead is a named table.
 # - Use try-except to verify lookahead validity and raise errors.
 # - Identify number of unique wells in the lookahead.
-# - For lines after Actual, fill with AFE Time. For lines without AFE Time, fill with DSV Time.
-# - Recalculate Start Time after Actual.
+# - Identify lookahead start time.
+# - Compute Projection Time based on Actual Time, then AFE Time, then DSV Time.
+# - Recalculate Projection Start Time based on Projection Time.
 # - For each well, generate Performance Tracker.
 
 # Proposed verification:
 # - Lookahead name, sheet name, and table name are as expected.
 # - All expected Phase Codes and Phases for each well are present.
+# - Raise warning if there are gaps in Actual Time.
 
 
 def read_lookahead():
@@ -41,3 +43,29 @@ def read_lookahead():
     rows_list = [[col.value for col in row] for row in data]
     df = pd.DataFrame(data=rows_list[1:], index=None, columns=rows_list[0])
     return df
+
+
+# Load workbook into dataframe.
+df_lookahead = read_lookahead()
+
+# Rename headers and remove unnecessary columns.
+df_lookahead = df_lookahead.rename({'GK WELL OPERATIONS LOOKAHEAD': 'Description',
+                                    'DWOP': 'AFE Time',
+                                    'DSV plan': 'DSV Time',
+                                    'Actual \nTime': 'Actual Time'}, axis='columns')
+df_lookahead = df_lookahead[['Start Time', 'Well Name', 'Phase Code', 'Phase', 'Description', 'AFE Time', 'DSV Time',
+                             'Actual Time']]
+
+# Identify number of unique wells in the lookahead.
+for well in df_lookahead['Well Name'].unique():
+    if well is not None:
+        pass
+
+# Identify lookahead start time.
+lookahead_start_time = df_lookahead['Start Time'].iloc[0]
+
+# Recalculate projection start time.
+df_lookahead['Projection Time'] = df_lookahead['Actual Time'].fillna(df_lookahead['AFE Time']).fillna(df_lookahead['DSV Time'])
+df_lookahead['Cumulative Projection Time'] = df_lookahead['Projection Time'].cumsum().shift(fill_value=0)
+df_lookahead['Projection Start Time'] = df_lookahead['Cumulative Projection Time'].apply(lambda x: lookahead_start_time + pd.Timedelta(hours=x))
+df_lookahead['Projection End Time'] = df_lookahead['Projection Start Time'] + pd.to_timedelta(df_lookahead['Projection Time'], unit='hours')
